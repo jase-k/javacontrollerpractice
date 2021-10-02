edc8772389c7ab4da8121d691eb538a84102646e
Jase Kraft <jase@perfectmypodcast.com>
2021-10-02 06:51:27 -0500

    Practice with PathVariables

--- src/main/java/com/jasekraft/daikichi/DaikichiRoutes.java
+++ src/main/java/com/jasekraft/daikichi/DaikichiRoutes.java
@@ -1,5 +1,6 @@
 package com.jasekraft.daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +22,18 @@ public class DaikichiRoutes {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
 	
+	@RequestMapping("/travel/{place}")
+	public String showDestination(@PathVariable("place") String destination) {
+		return "Have fun in " + destination;
+	}
+	
+	@RequestMapping("/lotto/{guess}")
+	public String participateInLott(@PathVariable("guess") int guess) {
+		if(guess%2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor  but now is a great time to spend time with family and friends.";
+		}
+	}
 }
